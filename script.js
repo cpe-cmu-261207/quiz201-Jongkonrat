999812354d25b6ab5027543f4362b3b36e3e26f7
Jongkonrat <jongkonrat_pisit@cmu.ac.th>
2021-07-29 14:05:20 +0700

    ....

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+const btn_search = document.getElementById('search')
+const btn_reset = document.getElementById('reset')
 
 btn_toggle.onclick = () => {
   // your code here
-  const Nocalculation
+  const Nocalculation;
   Nocalculation =  630610722+length;
-  document.append(Nocalculation)
+  document.append(Nocalculation);
 }
 
 // more codes for Search and Reset buttons here
+btn_search.onclick = () => {
+
+}
+
+btn_reset.onclick = () => {
+  
+}
